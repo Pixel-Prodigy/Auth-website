f5014e52edb3872a63e12a01d1f47cd4c4015d7a
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-21 02:31:37 +0530

    added src/components/layout/Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   const { data: session } = useSession();
   const pathname = usePathname();
   return (
-    <header className="flex items-center justify-center gap-4 pt-6 text-gray-400 font-semibold text-lg">
+    <header className="flex items-center justify-center absolute top-0 left-0 right-0 gap-4 pt-6 text-gray-400 font-semibold text-lg">
       <Link
         href="/"
         className={`transition-colors duration-500 hover:text-gray-300 ${
@@ -23,14 +23,29 @@ export default function Header() {
       >
         About
       </Link>
+      <Link
+        href="/anime"
+        className={`transition-colors duration-500 hover:text-gray-300 ${
+          pathname === "/anime" ? " text-white border-b-2 pb-1" : ""
+        }`}
+      >
+        Anime
+      </Link>
       {session ? (
         <Link
           href="/login"
-          className={`transition-colors duration-300 hover:text-gray-300 ${
+          className={`transition-colors duration-300 hover:text-gray-300 flex items-center gap-2 ${
             pathname === "/login" ? " text-white border-b-2 pb-1" : ""
           }`}
         >
-          {session.user?.name}
+          <p>{session.user?.name}</p>
+          {session.user?.image && (
+            <img
+              src="{session.user?.image}"
+              className="w-8 h-8 z-10 rounded-full"
+              alt=""
+            />
+          )}
         </Link>
       ) : (
         <Link
