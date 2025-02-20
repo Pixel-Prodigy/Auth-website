0de08320d81ea34a8216517705c7b37d7e49bd2b
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-21 00:27:16 +0530

    dibugg

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function About() {
     <section className="flex flex-col gap-10 pt-24 items-center justify-center">
       <h1 className="text-6xl text-white font-bold tracking-wide">About Us</h1>
       <article className="text-lg text-gray-400 max-w-2xl text-center">
-        Welcome to our anime hub! We bring you the best anime quotes, character insights, and discussions. Whether you're a casual watcher or a hardcore otaku, our platform is designed to fuel your anime passion.
+        Welcome to our anime hub! We bring you the best anime quotes, character insights, and discussions. Whether you&apos;re a casual watcher or a hardcore otaku, our platform is designed to fuel your anime passion.
       </article>
       <div className="flex flex-col gap-4">
         <h2 className="text-3xl font-semibold text-white">Why Choose Us?</h2>
