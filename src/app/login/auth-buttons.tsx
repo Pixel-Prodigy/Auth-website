c0169e79280aefa8d0f3f6d88cf96bc9aa6bef73
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-21 02:31:26 +0530

    added src/app/login/auth-buttons.tsx

--- src/app/login/auth-buttons.tsx
+++ src/app/login/auth-buttons.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 export function SignInButton() {
   return (
-    <Button
-      className="w-full max-w-40 mx-auto rounded text-md font-semibold"
-      onClick={() => signIn("github")}
-    >
-      Sign in with <FaGithub className="ml-2" />
-    </Button>
+    <>
+      <Button
+        className="w-full max-w-40 mx-auto rounded text-md font-semibold"
+        onClick={() => signIn("github")}
+      >
+        Sign in with <FaGithub className="ml-2" />
+      </Button>
+      <Button
+        className="w-full max-w-40 mx-auto rounded text-md font-semibold mt-2"
+        onClick={() => signIn("google")}
+      >
+        Sign in with <FaGoogle className="ml-2" />
+      </Button>
+    </>
   );
 }
 
