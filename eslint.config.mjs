68b8871eccc3a1416e2f8a1ddcca90a3f75e2b82
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-21 03:51:21 +0530

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default [
   {
     rules: {
       quotes: "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,4 @@
 import { FaCheck } from "react-icons/fa";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function About() {
   return (
