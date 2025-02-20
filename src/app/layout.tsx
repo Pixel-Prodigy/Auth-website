f79b121d008144f2a3c6e5fdf04a7f3e6243e84f
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-21 02:31:23 +0530

    added src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`bgColor ${geistMono.variable}`}>
+    <html lang="en" className="h-full">
+      <body className={`bgColor h-full ${geistMono.variable}`}>
         <AuthProvider>
           <Header />
 
