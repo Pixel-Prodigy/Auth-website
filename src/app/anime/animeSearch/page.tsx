108ab9e57f3f3bc6e52d7bc037aaa0029128e057
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-22 00:32:48 +0530

    Updated animeSearch/page.tsx

--- src/app/anime/animeSearch/page.tsx
+++ src/app/anime/animeSearch/page.tsx
@@ -1,9 +1,17 @@
-import AnimeData from "./AnimeData";
-import {AnimeProvider} from "./Context";
-export default function AnimeSearch() {
+import { redirect } from "next/navigation";
+import SearchFetch from "./SearchFetch";
+import { getServerSession } from "next-auth/next";
+
+export default async function AnimeSearch() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
-    <AnimeProvider>
-      <AnimeData />
-    </AnimeProvider>
+    <div>
+      <SearchFetch />
+    </div>
   );
 }
