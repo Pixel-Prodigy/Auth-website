817775bfa82c3ab37b3010bec724dedbafc06d6d
Pixel-Prodigy <gurumarkanyt@gmail.com>
2025-02-22 05:35:27 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,42 +2,46 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 import InteractiveCards from "../ui/InteractiveCards";
+
 export default function Header() {
   const { data: session } = useSession();
   const pathname = usePathname();
+
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "About", href: "/about" },
+    { name: "Anime", href: "/anime" },
+    { name: "Login", href: "/login" },
+  ];
+
   return (
-    <header className="flex items-center justify-center absolute top-0 left-0 right-0 gap-4 pt-6 text-gray-400 font-semibold text-lg">
-      <Link
-        href="/"
-        className={`transition-colors duration-500 hover:text-gray-300 ${
-          pathname === "/" ? " text-white border-b-2 pb-1" : ""
-        }`}
-      >
-        Home
-      </Link>
-      <Link
-        href="/about"
-        className={`transition-colors duration-500 hover:text-gray-300 ${
-          pathname === "/about" ? " text-white border-b-2 pb-1" : ""
-        }`}
-      >
-        About
-      </Link>
-      <Link
-        href="/anime"
-        className={`transition-colors duration-500 hover:text-gray-300 ${
-          pathname === "/anime" ? " text-white border-b-2 pb-1" : ""
-        }`}
-      >
-        Anime
-      </Link>
-      {session ? (
+    <header style={{ borderRadius: "8px" }} className="flex items-center justify-center w-fit mx-auto px-5 bg-[#0f172a] h-[45px] absolute top-2 left-0 right-0 gap-4 pt-6 text-gray-400 font-semibold text-lg">
+      <div className="relative flex -translate-y-3 gap-6">
+        {links.map(({ name, href }) => (
+          <Link
+            key={href}
+            href={href}
+            className="relative transition-colors duration-500 hover:text-gray-300"
+          >
+            {name}
+            {pathname === href && (
+              <motion.div
+                layoutId="active-link"
+                className="absolute -bottom-[1px]  -right-2 h-[31px] w-[70px] bg-gray-400/40 rounded-md"
+                style={{ borderRadius: "8px" }}
+                transition={{ type: "spring", stiffness: 300, damping: 30 }}
+              />
+            )}
+          </Link>
+        ))}
+      </div>
+
+      {session && (
         <Link
           href="/login"
-          className={`transition-colors duration-300 hover:text-gray-300 ${
-            pathname === "/login" ? " text-white border-b-2 pb-1" : ""
-          }`}
+          className="transition-colors duration-300 -translate-y-3 hover:text-gray-300"
         >
           <div className="flex items-center gap-2">
             {session.user?.image && (
@@ -50,15 +54,6 @@ export default function Header() {
               />
             )}
           </div>
-        </Link>
-      ) : (
-        <Link
-          href="/login"
-          className={`transition-colors duration-500 hover:text-gray-300 ${
-            pathname === "/login" ? " text-white border-b-2 pb-1" : ""
-          }`}
-        >
-          Login
         </Link>
       )}
     </header>
